d808e4df32e4ed7c6bf277361a8fe7d338ba41d6
NeeloBee <boineelolk@gmail.com>
2021-10-22 16:06:02 +0200

    references to buttons added#1

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,6 +1,22 @@
+//get a reference to the add button
+const addButton = document.querySelector(".addButton");
+//get a reference to the minus button
+const minusButton = document.querySelector(".minusButton");
+//get a reference to the remove button
+const removeButton = document.querySelector(".removeButton");
+
+//checkout button
+const checkoutButton = document.querySelector(".Button");
+//order button
+
+//display message saying "Sorry that is not enough money when too little money is put"
+
+
+//display message saying "Enjoy your pizza when the right amount is inputted"
+
 //to get the value of the element
 const theElement = document.querySelector(".targetElement");
-console.log(theElement.value);
+//console.log(theElement.value);
 
 //to set a new value in an event
 theElement.value = "The new value..."
